99e6f73c44c9e706eac726cbbd6d7ebbea13aaa5
Chairoo <dacpanorunel@gmail.com>
2024-08-12 11:08:48 +0800

    Latest Version

--- seleniumtest1/src/main/java/seleniumtest1/App.java
+++ seleniumtest1/src/main/java/seleniumtest1/App.java
@@ -1,13 +1,18 @@
 package seleniumtest1;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class App {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.edgedriver().setup();
+        WebDriver driver = new EdgeDriver();
+        driver.get("https://www.facebook.com");
+        Thread.sleep(1000);
+        driver.quit();
+
     }
+
 }
