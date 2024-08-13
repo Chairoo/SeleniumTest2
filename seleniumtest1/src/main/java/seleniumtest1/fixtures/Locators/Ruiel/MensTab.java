100ea4132b1ba22057c4a0bc205c41ab9f7a1221
AyelChairo <ruielchairod@gmail.com>
2024-08-13 15:16:55 +0800

    Finished MensTab locators

--- seleniumtest1/src/main/java/seleniumtest1/fixtures/Locators/Ruiel/MensTab.java
+++ seleniumtest1/src/main/java/seleniumtest1/fixtures/Locators/Ruiel/MensTab.java
@@ -54,8 +54,68 @@ public class MensTab {
     //BODY
     //images
     public static final String btn_Image1 = "//span[text()='Luma’s Performance Fabric collection']";
-    public static final String btn_Image2 = "//span[text()='Luma’s Performance Fabric collection']";
+    public static final String btn_Image2 = "//span[text()='Buy 3 Luma tees, get 4 instead']";
+    public static final String btn_Image3 = "//strong[text()='Last chance']";
+    public static final String btn_Image4 = "//strong[text()='Luma shorts']";
+    public static final String btn_Image5 = "//strong[text()='Luma tees']";
+    public static final String btn_Image6 = "//strong[text()='Luma hoodies']";
 
+    //HOT SELLERS = HS, Favorites from Luna shoppers
+    //Argus All-Weather Tank
+    public static final String btn_HS1 = "//a[@title='Argus All-Weather Tank']";
+    public static final String btn_HS1_Image = "//img[@alt='Argus All-Weather Tank']";
+    public static final String btn_HS1_XS = "//*[@id='maincontent']/div[4]/div[1]/div[1]/div[3]/div/div/ol/li[1]/div/div/div[2]/div[1]/div/div[1]";
+    public static final String btn_HS1_S = "//*[@id='maincontent']/div[4]/div[1]/div[1]/div[3]/div/div/ol/li[1]/div/div/div[2]/div[1]/div/div[2]";
+    public static final String btn_HS1_M = "//*[@id='maincontent']/div[4]/div[1]/div[1]/div[3]/div/div/ol/li[1]/div/div/div[2]/div[1]/div/div[3]";
+    public static final String btn_HS1_L = "//*[@id='maincontent']/div[4]/div[1]/div[1]/div[3]/div/div/ol/li[1]/div/div/div[2]/div[1]/div/div[4]";
+    public static final String btn_HS1_XL = "//*[@id='maincontent']/div[4]/div[1]/div[1]/div[3]/div/div/ol/li[1]/div/div/div[2]/div[1]/div/div[5]";
+    public static final String btn_HS1_Color1 = "//*[@id='option-label-color-93-item-52']";
+    public static final String btn_HS1_AddtoCart = "//form[@data-product-sku='MT07']";
+    public static final String btn_HS1_WishList = "//*[@id='maincontent']/div[4]/div[1]/div[1]/div[3]/div/div/ol/li[1]/div/div/div[3]/div/div[2]/a[1]";
+    public static final String btn_HS1_Compare = "//*[@id='maincontent']/div[4]/div[1]/div[1]/div[3]/div/div/ol/li[1]/div/div/div[3]/div/div[2]/a[2]";
+
+    //Hero Hoodie
+    public static final String btn_HS2 = "//a[@title='Hero Hoodie']";
+    public static final String btn_HS2_Image = "//img[@alt='Hero Hoodie']";
+    public static final String btn_HS2_XS = "//*[@id='maincontent']/div[4]/div[1]/div[1]/div[3]/div/div/ol/li[2]/div/div/div[2]/div[1]/div/div[1]";
+    public static final String btn_HS2_S = "//*[@id='maincontent']/div[4]/div[1]/div[1]/div[3]/div/div/ol/li[2]/div/div/div[2]/div[1]/div/div[2]";
+    public static final String btn_HS2_M = "//*[@id='maincontent']/div[4]/div[1]/div[1]/div[3]/div/div/ol/li[2]/div/div/div[2]/div[1]/div/div[3]";
+    public static final String btn_HS2_L = "//*[@id='maincontent']/div[4]/div[1]/div[1]/div[3]/div/div/ol/li[2]/div/div/div[2]/div[1]/div/div[4]";
+    public static final String btn_HS2_XL = "//*[@id='maincontent']/div[4]/div[1]/div[1]/div[3]/div/div/ol/li[2]/div/div/div[2]/div[1]/div/div[5]";
+    public static final String btn_HS2_Color1 = "//*[@id='maincontent']/div[4]/div[1]/div[1]/div[3]/div/div/ol/li[2]/div/div/div[2]/div[2]/div/div[1]";
+    public static final String btn_HS2_Color2 = "//*[@id='maincontent']/div[4]/div[1]/div[1]/div[3]/div/div/ol/li[2]/div/div/div[2]/div[2]/div/div[2]";
+    public static final String btn_HS2_Color3 = "//*[@id='maincontent']/div[4]/div[1]/div[1]/div[3]/div/div/ol/li[2]/div/div/div[2]/div[2]/div/div[3]";
+    public static final String btn_HS2_AddtoCart = "//form[@data-product-sku='MH07']";
+    public static final String btn_HS2_WishList = "//*[@id='maincontent']/div[4]/div[1]/div[1]/div[3]/div/div/ol/li[2]/div/div/div[3]/div/div[2]/a[1]";
+    public static final String btn_HS2_Compare = "//*[@id='maincontent']/div[4]/div[1]/div[1]/div[3]/div/div/ol/li[2]/div/div/div[3]/div/div[2]/a[2]";
+
+    //Meteor Workout Short
+    public static final String btn_HS3 = "//a[@title='Meteor Workout Short']";
+    public static final String btn_HS3_Image = "//img[@alt='Meteor Workout Short']";
+    public static final String btn_HS3_32 = "//*[@id='maincontent']/div[4]/div[1]/div[1]/div[3]/div/div/ol/li[3]/div/div/div[3]/div[1]/div/div[1]";
+    public static final String btn_HS3_33 = "//*[@id='maincontent']/div[4]/div[1]/div[1]/div[3]/div/div/ol/li[3]/div/div/div[3]/div[1]/div/div[2]";
+    public static final String btn_HS3_34 = "//*[@id='maincontent']/div[4]/div[1]/div[1]/div[3]/div/div/ol/li[3]/div/div/div[3]/div[1]/div/div[3]";
+    public static final String btn_HS3_36 = "//*[@id='maincontent']/div[4]/div[1]/div[1]/div[3]/div/div/ol/li[3]/div/div/div[3]/div[1]/div/div[4]";
+    public static final String btn_HS3_Color1 = "//*[@id='maincontent']/div[4]/div[1]/div[1]/div[3]/div/div/ol/li[3]/div/div/div[3]/div[2]/div/div[1]";
+    public static final String btn_HS3_Color2 = "//*[@id='maincontent']/div[4]/div[1]/div[1]/div[3]/div/div/ol/li[3]/div/div/div[3]/div[2]/div/div[2]";
+    public static final String btn_HS3_Color3 = "//*[@id='maincontent']/div[4]/div[1]/div[1]/div[3]/div/div/ol/li[3]/div/div/div[3]/div[2]/div/div[3]";
+    public static final String btn_HS3_AddtoCart = "//form[@data-product-sku='MSH03']";
+    public static final String btn_HS3_WishList = "//*[@id='maincontent']/div[4]/div[1]/div[1]/div[3]/div/div/ol/li[3]/div/div/div[4]/div/div[2]/a[1]";
+    public static final String btn_HS3_Compare = "//*[@id='maincontent']/div[4]/div[1]/div[1]/div[3]/div/div/ol/li[3]/div/div/div[4]/div/div[2]/a[2]";
+    
+    //Geo Insulated Jogging Pant
+    public static final String btn_HS4 = "//a[@title='Geo Insulated Jogging Pant']";
+    public static final String btn_HS4_Image = "//img[@alt='Geo Insulated Jogging Pant']";
+    public static final String btn_HS4_32 = "//*[@id='maincontent']/div[4]/div[1]/div[1]/div[3]/div/div/ol/li[4]/div/div/div[3]/div[1]/div/div[1]";
+    public static final String btn_HS4_33 = "//*[@id='maincontent']/div[4]/div[1]/div[1]/div[3]/div/div/ol/li[4]/div/div/div[3]/div[1]/div/div[2]";
+    public static final String btn_HS4_34 = "//*[@id='maincontent']/div[4]/div[1]/div[1]/div[3]/div/div/ol/li[4]/div/div/div[3]/div[1]/div/div[3]";
+    public static final String btn_HS4_36 = "//*[@id='maincontent']/div[4]/div[1]/div[1]/div[3]/div/div/ol/li[4]/div/div/div[3]/div[1]/div/div[4]";
+    public static final String btn_HS4_Color1 = "//*[@id='maincontent']/div[4]/div[1]/div[1]/div[3]/div/div/ol/li[4]/div/div/div[3]/div[2]/div/div[1]";
+    public static final String btn_HS4_Color2 = "//*[@id='maincontent']/div[4]/div[1]/div[1]/div[3]/div/div/ol/li[4]/div/div/div[3]/div[2]/div/div[2]";
+    public static final String btn_HS4_Color3 = "//*[@id='maincontent']/div[4]/div[1]/div[1]/div[3]/div/div/ol/li[4]/div/div/div[3]/div[2]/div/div[3]";
+    public static final String btn_HS4_AddtoCart = "//form[@data-product-sku='MP03']";
+    public static final String btn_HS4_WishList = "//*[@id='maincontent']/div[4]/div[1]/div[1]/div[3]/div/div/ol/li[4]/div/div/div[4]/div/div[2]/a[1]";
+    public static final String btn_HS4_Compare = "//*[@id='maincontent']/div[4]/div[1]/div[1]/div[3]/div/div/ol/li[4]/div/div/div[4]/div/div[2]/a[2]";
     
     // Checking if the element is existing
     public static void main(String[] args) {
